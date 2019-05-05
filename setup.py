646bcaedcc438e77387cbdcbb3ee55bc16e306ee
Adriaan Knapen <hi@aknapen.nl>
2019-05-05 14:44:11 +0300

    fix: typo in repository url

--- setup.py
+++ setup.py
@@ -1,12 +1,29 @@
+import os.path
 from setuptools import setup, find_packages
 
-setup(name='mindconnectiot',
-      version='0.1',
+# The directory containing this file
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+# The text of the README file
+with open(os.path.join(HERE, "README.md")) as fid:
+    README = fid.read()
+
+setup(
+      name='mindconnectiot',
+      version='0.1.6',
       description='Wrapper around the MindConnect IoT Extention.',
-      url='https://gitlab.com/Addono/mindconnect-iot-extention-python',
+	  long_description=README,
+	  long_description_content_type='text/markdown',
+      url='https://gitlab.com/Addono/mindconnect-iot-extension-python',
       author='Adriaan Knapen',
       author_email='hi@aknapen.nl',
       license='MIT',
       packages=find_packages(),
+	  include_package_data=True,
+	  classifiers=[
+          "License :: OSI Approved :: MIT License",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.7",
+      ],
       zip_safe=False,
       install_requires=['paho-mqtt'])
